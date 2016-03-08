4627012e56013b4d3db8d4cbee70807eb136281a
Andreas Koller <mail@andreaskoller.com>
2016-03-08 15:48:31 +0100

    Smaller heading, no bg of icon

--- src/components/WhatsnewListitem.js
+++ src/components/WhatsnewListitem.js
@@ -39,10 +39,10 @@ export default React.createClass({
     var divClass = classNames( 'box white linked text-center whatsnew' );
 
     if(this.props.data.activity.id) {
-      var icon = <Icon type={'activity-' + this.props.data.activity.type} area='whatsnew' shape='hexagon'/>
+      var icon = <Icon type={'activity-' + this.props.data.activity.type} area='whatsnew' shape='empty'/>
     }
     else {
-      var icon = <Icon type='whatsnew' area='whatsnew' shape='hexagon'/>
+      var icon = <Icon type='whatsnew' area='whatsnew' shape='empty'/>
     }
 
     return (
@@ -53,11 +53,11 @@ export default React.createClass({
 
           {icon}
 
-          <h2><FormattedMessage id={ 'whatsnew-' + this.props.data.type.replace(/\s+/g, '') } values={{
+          <h3><FormattedMessage id={ 'whatsnew-' + this.props.data.type.replace(/\s+/g, '') } values={{
               personName: this.props.data.person.name,
               activityName: this.props.data.activity.name || '',
               groupName: this.props.data.group.name || '',
-            }} defaultMessage=' '/></h2>
+            }} defaultMessage=' '/></h3>
 
           <p>
             <span className="grey"><FormattedRelative value={this.props.data.date} /></span>
