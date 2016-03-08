b2055747be09b072dfd4ef1979db9bc8262d5272
Andreas Koller <mail@andreaskoller.com>
2016-03-08 15:47:09 +0100

    Changed storing of activitiyId in photos

--- src/stores/DataStore.js
+++ src/stores/DataStore.js
@@ -192,14 +192,14 @@ export default Reflux.createStore({
         view: "Main View"
       }).eachPage(function page(records, fetchNextPage) {
         records.forEach(function(record) {
-            if (record.get('Nr') && record.get('Image') && record.get('Image').length > 0) {
+            if (record.get('Nr') && record.get('Activity') && record.get('Image') && record.get('Image').length > 0) {
               data.photos.push({
                 id: record.getId(),
                 nr: record.get('Nr'),
                 image: record.get('Image') || [],
                 description: record.get('Description') || '',
-                ownerId: record.get('Owner'),
-                activities: record.get('Activities')
+                ownerId: record.get('Owner') || '',
+                activityId: record.get('Activity')[0] || ''
               });
             }
         });
