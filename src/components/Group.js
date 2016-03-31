9034725500c5699ead053d0934dd627b444d5b63
Andreas Koller <mail@andreaskoller.com>
2016-03-31 18:59:49 +0200

    Display group name

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,11 +1,40 @@
 import React from 'react';
 
+import Reflux from 'reflux';
+import LanguageActions from '../stores/LanguageActions';
+import LanguageStore from '../stores/LanguageStore';
+import DataActions from '../stores/DataActions';
+import DataStore from '../stores/DataStore';
+import StatusActions from '../stores/StatusActions';
+import StatusStore from '../stores/StatusStore';
+import Helpers from '../stores/Helpers.js';
+
 export default React.createClass({
+
+  mixins: [ Reflux.connect(LanguageStore, 'language'), Reflux.connect(DataStore, 'data'), Reflux.connect(StatusStore, 'status') ],
+
+  componentDidMount() {
+    LanguageStore.forceTrigger();
+    DataActions.forceTrigger();
+    StatusActions.forceTrigger();
+    StatusActions.setCurrentPage('');
+  },
+
   render() {
+
+    if (!Helpers.checkLanguageLoaded(this) || !this.props.params.id || !this.state.data || !this.state.data.loaded.all) {
+      return <div></div>;
+    }
+
+    var community = Helpers.getCommunityFromStatus(this);
+
+    var group = Helpers.getGroupById(this.props.params.id, this);
+
+
     return (
         <div className="jumbotron">
           <div className="container text-center">
-            <h1>Group</h1>
+            <h1>Group {group.name}</h1>
           </div>
         </div>
     );
