726b306f7c66a79e0381ec3bfebbd068f5c7525e
Andreas Koller <mail@andreaskoller.com>
2016-03-31 19:00:58 +0200

    Renamed Stream to Photowall

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -127,7 +127,7 @@ export default React.createClass({
         <Row>
           <Col md={12} className="text-center box">
             <ButtonGroup>
-              <Button bsSize="large" className="padded" active={ this.state.area === 'overview' } onClick={ this.setArea.bind(this, 'overview') }>Stream</Button>  
+              <Button bsSize="large" className="padded" active={ this.state.area === 'overview' } onClick={ this.setArea.bind(this, 'overview') }>Photowall</Button>  
               <Button bsSize="large" className="padded" active={ this.state.area === 'byactivity' } onClick={ this.setArea.bind(this, 'byactivity') }>By activity</Button>
               <Button bsSize="large" className="padded" active={ this.state.area === 'bytype' } onClick={ this.setArea.bind(this, 'bytype') }>By type</Button>  
             </ButtonGroup>
