21129d4e58d85fb6d73aa576387cd690ba8eb95f
Andreas Koller <mail@andreaskoller.com>
2016-04-08 14:11:31 +0200

    Avoid showing component before data is loaded

--- src/components/Agenda/AgendaListitem.js
+++ src/components/Agenda/AgendaListitem.js
@@ -21,18 +21,20 @@ export default React.createClass({
 
   render() {
 
-    if (this.state.data && this.state.data.loaded.all) {
 
-      var group = Helpers.getGroupById(this.props.data.groupId, this);
-      var owner = Helpers.getPersonById(group.ownerId, this);
-
-      var type = Helpers.getActivityTypeById(this.props.data.typeId, this);
-      var icon = <Icon type={'activity-' + type.name} area='agenda' fill='solid' shape='hexagon'/>;
-
-      // &nbsp;<span className="grey">(<FormattedRelative value={this.props.data.date} />)</span>
-      //  <p><FormattedMessage id="group" defaultMessage="Group"/> {group.name}
-      // &nbsp;<FormattedMessage id="by" defaultMessage="by"/> {owner.name}</p>
+    if (!this.state.data || !this.state.data.loaded.all) {
+      return <div></div>
     }
+
+    var group = Helpers.getGroupById(this.props.data.groupId, this);
+    var owner = Helpers.getPersonById(group.ownerId, this);
+
+    var type = Helpers.getActivityTypeById(this.props.data.typeId, this);
+    var icon = <Icon type={'activity-' + type.name} area='agenda' fill='solid' shape='hexagon'/>;
+
+    // &nbsp;<span className="grey">(<FormattedRelative value={this.props.data.date} />)</span>
+    //  <p><FormattedMessage id="group" defaultMessage="Group"/> {group.name}
+    // &nbsp;<FormattedMessage id="by" defaultMessage="by"/> {owner.name}</p>
 
     if (this.props.showDate) {
       var date = <p>
