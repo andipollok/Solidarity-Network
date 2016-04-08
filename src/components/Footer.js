c97b77a87ba9520c050da66a1c670f44b42470a1
Andreas Koller <mail@andreaskoller.com>
2016-04-08 12:59:20 +0200

    Hide settings link for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {Link}  from 'react-router';
 
 export default React.createClass({
+
   render() {
+
+    var settings = <p><Link to="/settings">Settings</Link></p>
+
     return (
       <footer>
-        <p><Link to="/settings">Settings</Link></p>
+
       </footer>
     );
   }
