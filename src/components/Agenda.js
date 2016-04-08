ac26497851719397d011ba3b415ebc8f34736aae
Andreas Koller <mail@andreaskoller.com>
2016-04-08 13:02:04 +0200

    Title change

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -29,7 +29,7 @@ export default React.createClass({
 
   componentWillMount() {
     StatusActions.historyAdd({
-      title: 'Schedule',
+      title: 'Agenda',
       url: '',
       pathname: '/agenda'
     });
