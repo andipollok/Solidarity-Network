8101dcc640919441848f349bd740f32faad05a9c
Andreas Koller <mail@andreaskoller.com>
2016-04-08 14:11:04 +0200

    Make activity types selectable

--- src/components/Agenda/ActivityTypeSelector.js
+++ src/components/Agenda/ActivityTypeSelector.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from 'moment';
+
 import classNames from 'classnames';
 import { FormattedMessage } from 'react-intl';
 import { Col, Row } from 'react-bootstrap';
@@ -8,6 +10,8 @@ import DataActions from '../../stores/DataActions';
 import DataStore from '../../stores/DataStore';
 import LanguageActions from '../../stores/LanguageActions';
 import LanguageStore from '../../stores/LanguageStore';
+import StatusActions from '../../stores/StatusActions';
+import StatusStore from '../../stores/StatusStore';
 import Helpers from '../../stores/Helpers.js';
 
 import Icon from '../General/Icon';
@@ -15,11 +19,12 @@ import Icon from '../General/Icon';
 
 export default React.createClass({
 
-  mixins: [ Reflux.connect(DataStore, 'data'), Reflux.connect(LanguageStore, 'language') ],
+  mixins: [ Reflux.connect(DataStore, 'data'), Reflux.connect(LanguageStore, 'language'), Reflux.connect(StatusStore, 'status') ],
 
   componentDidMount() {
     DataActions.forceTrigger();
     LanguageActions.forceTrigger();
+    StatusActions.forceTrigger();
   },
 
   onClickType(id) {
@@ -33,16 +38,55 @@ export default React.createClass({
     if (!Helpers.checkLanguageLoaded(this) || !this.state.data || !this.state.data.loaded.activitytypes) {
       return <div></div>;
     }
+    
+    var types = this.state.data.activitytypes;
+
+    if (this.state.data && this.state.data.loaded.all && this.state.status && this.state.status.community) {
+
+      var activities = this.state.data.activities.filter(
+        function(activity) {
+
+          // check if activity is in selected community
+          var _group = Helpers.getGroupById(activity.groupId, this);
+          if (!_group) {
+            return false;
+          }
+          var _community = Helpers.getCommunityById(_group.communityId, this);
+          if (_community.id !== this.state.status.community) {
+            return false; // filter this entry if item is not in the community
+          }
+          // check if activity is in the past
+          if (moment(activity.date) < moment()) {
+            return false;
+          }
+
+          return true;
+        }.bind(this)
+      );
+
+      for (var type of types) {
+        type.count = 0;
+        activities.map(function(activity) {
+          // check if has selected type
+          if (type.id === activity.typeId) {
+            type.count++;
+          }
+        });
+      }
+
+      types.sort((a, b) => b.count - a.count);
+      
+    }
 
     var typeItem = function(type) {
-      if (this.props.selectedType === type.id) {
-        var selected = "selected";
-      }
+      var divClass = classNames('text-center', {
+        'selected': this.props.selectedType === type.id
+      });
       return ( 
-        <li key={type.id} onClick={this.onClickType.bind(this, type.id)} className="text-center">
+        <li key={type.id} onClick={this.onClickType.bind(this, type.id)} className={divClass}>
           <p><Icon type={'activity-' + type.name} area='agenda' shape='hexagon'/></p>
           <p>{type.name}</p>
-          {selected}
+          {type.count}
         </li>
       );
     }.bind(this);
@@ -50,7 +94,7 @@ export default React.createClass({
     return (
       <div className="activity-type-list">
         <ul>
-          {this.state.data.activitytypes.map(typeItem)}
+          {types.map(typeItem)}
         </ul>
       </div>
     );
