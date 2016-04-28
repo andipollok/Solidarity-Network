ba8519b64e6217fa797d14b9e0b459eaa6ab9d6e
Andreas Koller <mail@andreaskoller.com>
2016-04-28 17:40:13 +0200

    Show start and end time

--- src/components/Agenda/AgendaListitem.js
+++ src/components/Agenda/AgendaListitem.js
@@ -21,22 +21,22 @@ export default React.createClass({
 
   render() {
 
-
     if (!this.state.data || !this.state.data.loaded.all) {
       return <div></div>
     }
 
     var group = Helpers.getGroupById(this.props.data.groupId, this);
-
     var type = Helpers.getActivityTypeById(this.props.data.typeId, this);
-    var icon = <IconActivity type={type} area='agenda' fill='solid' />;
+    
+    // check if activity is in the past          
+    var isInPast = new Date(this.props.data.date) < new Date();
 
     // &nbsp;<span className="grey">(<FormattedRelative value={this.props.data.date} />)</span>
     //  <p><FormattedMessage id="group" defaultMessage="Group"/> {group.name}
     // &nbsp;<FormattedMessage id="by" defaultMessage="by"/> {owner.name}</p>
 
     if (this.props.showDate) {
-      var date = <p>
+      var componentDate = <p>
           <FormattedMessage id="on" defaultMessage="on"/>
           &nbsp;
           <FormattedDate
@@ -48,14 +48,50 @@ export default React.createClass({
         </p>
     }
 
+    if (this.props.showIcon) {
+      var componentIcon = <IconActivity type={type} area='agenda' fill='solid' />;
+    }
+
     if (this.props.showTime) {
-      var time = <p>
+
+      var startingAt;
+
+      if (isInPast === false) {
+        // event is in the future
+        startingAt = <FormattedMessage id="startingat" defaultMessage=" "/>
+      } 
+      else {
+        // event is in the past
+        startingAt = <FormattedMessage id="startedat" defaultMessage=" "/>
+      }
+/*      var componentTime = <p>
             <FormattedMessage id="startingat" defaultMessage="Starting at"/>
             &nbsp;<FormattedTime
                     value={this.props.data.date}
                     minute="2-digit"
                     hour="numeric" />
-          </p>
+          </p>*/
+
+      // format start and end time
+      var componentTime = <p>
+                    {startingAt}&nbsp;<FormattedTime
+                          value={this.props.data.date}
+                          minute="2-digit"
+                          hour="numeric" />
+                          </p>
+
+      if (this.props.data.dateEnd) {
+        componentTime = <p>
+                        From&nbsp;<FormattedTime
+                          value={this.props.data.date}
+                          minute="2-digit"
+                          hour="numeric" />
+                          &nbsp;to&nbsp;<FormattedTime
+                          value={this.props.data.dateEnd}
+                          minute="2-digit"
+                          hour="numeric" />
+                          </p>
+      }
     }
 
     return (
@@ -64,14 +100,13 @@ export default React.createClass({
 
         <div className="card solid linked padded text-center solid">
 
-          {icon}
+          {componentIcon}
 
           <h2>{this.props.data.name}</h2>
 
-          {date}
+          {componentDate}
 
-          {time}
-
+          {componentTime}
 
         </div>
       </Col>
