6f5e663ecf0492c4f2975cae3037524d0b527c20
Andreas Koller <mail@andreaskoller.com>
2016-05-22 11:48:47 +0200

    Changed name to alo

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -25,7 +25,7 @@ export default React.createClass({
       <div className="container activities">
         <Row>
           <Col sm={12} className="text-center">
-            <h1>Salü!</h1>
+            <h1>alo</h1>
             <Button bsSize="large" className="startButton" onClick={this.onClickStart}>Get started!</Button>
           </Col>
         </Row>
