fa28b3c658cec3c1dda20e698a8d815cd2dff279
David Roulin <david.roulin@epfl.ch>
2016-06-03 10:07:41 +0200

    proof of concept

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -8,6 +8,12 @@ import StatusStore from '../../stores/StatusStore';
 
 export default React.createClass({
 
+  getInitialState() {
+    return {
+       loggedIn: false,
+    };
+  },
+
   componentWillMount() {
     StatusActions.setPage('start');
     StatusActions.setTitle(null);
@@ -19,17 +25,41 @@ export default React.createClass({
     window.location.assign('#/activities');
   },
 
+  onClickLogin() {
+    this.setState({ loggedIn: true });
+  },
+
   render() {
 
-    return (
-      <div className="container activities">
-        <Row>
-          <Col sm={12} className="text-center">
-            <h1>alo</h1>
-            <Button bsSize="large" className="startButton" onClick={this.onClickStart}>Get started!</Button>
-          </Col>
-        </Row>
-      </div>
-    );
+    if (this.state.loggedIn) {
+
+      return (
+        <div className="container activities">
+          <Row>
+            <Col sm={12} className="text-center">
+              <h1>alo</h1>
+              <Button bsSize="large" className="startButton" onClick={this.onClickStart}>Get started!</Button>
+            </Col>
+          </Row>
+        </div>
+      );
+
+    } else {
+
+      return (
+        <div className="container activities">
+          <Row>
+            <Col sm={12} className="text-center">
+              <h1>alo</h1>
+              You need to log in.
+              <br/>
+              <Button bsSize="large" className="loginButton" onClick={this.onClickLogin}>Log me in!</Button>
+            </Col>
+          </Row>
+        </div>
+      );
+      
+    }
+
   }
 });
