6544fab44fab442f7a80aac962d2ea119986b3d4
David Roulin <david.roulin@epfl.ch>
2016-08-24 20:18:47 +0200

    WIP start screen step by step render

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -16,6 +16,12 @@ import Icon from '../General/Icon';
 
 export default React.createClass({
 
+  getInitialState() {
+    return {
+      step: 1
+    };
+  },
+
   componentWillMount() {
     StatusActions.setPage('start');
     StatusActions.setTitle(null);
@@ -32,46 +38,89 @@ export default React.createClass({
     window.location.assign('#/stories');
   },
 
-
   render() {
 
-    return (
-      <div className="container-fluid start">
-        <Row>
-          <Col sm={12} className="text-center">
-            <p>
-              <ReactCSSTransitionGroup transitionName="move-up" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
-                  transitionAppearTimeout={2000}>
-                  <SvgIcon name='app/alo_logo' color='#FFFFFF'/>
-              </ReactCSSTransitionGroup>
-            </p>
+    switch ( this.state.step ) {
+      case 1:
+          return (
+            <div className="container-fluid start">
+              <Row>
+                <Col sm={12} className="text-center">
+                  <p>
+                    <ReactCSSTransitionGroup transitionName="move-up" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
+                        transitionAppearTimeout={2000}>
+                        <SvgIcon name='app/alo_logo' color='#FFFFFF'/>
+                    </ReactCSSTransitionGroup>
+                  </p>
 
-              <ReactCSSTransitionGroup transitionName="fade-in1" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
-                transitionAppearTimeout={5000}>
-                <p className="item">
-                  <Icon type='activity' folder='service' size='large' area='splash'/>
-                  <br />
-                  <Button bsSize="large" className="showActivities" onClick={this.onClickActivities}>
-                    <FormattedMessage id='takemetoactivities'/>
-                  </Button>
-                </p>
-              </ReactCSSTransitionGroup>
+                    <ReactCSSTransitionGroup transitionName="fade-in1" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
+                      transitionAppearTimeout={5000}>
+                      <p className="item">
+                        <Icon type='activity' folder='service' size='large' area='splash'/>
+                        <br />
+                        <Button bsSize="large" className="showActivities" onClick={this.onClickActivities}>
+                          <FormattedMessage id='takemetoactivities'/>
+                        </Button>
+                      </p>
+                    </ReactCSSTransitionGroup>
 
-              <ReactCSSTransitionGroup transitionName="fade-in2" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
-                transitionAppearTimeout={5000}>
-                <p className="item">
-                  <Icon type='story' folder='activities' size='large' area='splash'/>
-                  <br />
-                  <Button bsSize="large" className="showStories" onClick={this.onClickStories}>
-                    <FormattedMessage id='takemetostories'/>
-                  </Button>
-                </p>
-              </ReactCSSTransitionGroup>
-          </Col>
-        </Row>
-      </div>
-    );
+                    <ReactCSSTransitionGroup transitionName="fade-in2" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
+                      transitionAppearTimeout={5000}>
+                      <p className="item">
+                        <Icon type='story' folder='activities' size='large' area='splash'/>
+                        <br />
+                        <Button bsSize="large" className="showStories" onClick={this.onClickStories}>
+                          <FormattedMessage id='takemetostories'/>
+                        </Button>
+                      </p>
+                    </ReactCSSTransitionGroup>
+                </Col>
+              </Row>
+            </div>
+          );
+          break;
 
+      case 2:
+          return (
+            <div className="container-fluid start">
+              <Row>
+                <Col sm={12} className="text-center">
+                  <p>
+                    <ReactCSSTransitionGroup transitionName="move-up" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
+                        transitionAppearTimeout={2000}>
+                        <SvgIcon name='app/alo_logo' color='#FFFFFF'/>
+                    </ReactCSSTransitionGroup>
+                  </p>
+
+                    <ReactCSSTransitionGroup transitionName="fade-in1" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
+                      transitionAppearTimeout={5000}>
+                      <p className="item">
+                        <Icon type='activity' folder='service' size='large' area='splash'/>
+                        <br />
+                        <Button bsSize="large" className="showActivities" onClick={this.onClickActivities}>
+                          <FormattedMessage id='takemetoactivities'/>
+                        </Button>
+                      </p>
+                    </ReactCSSTransitionGroup>
+
+                    <ReactCSSTransitionGroup transitionName="fade-in2" transitionAppear={true} transitionEnterTimeout={0} transitionLeaveTimeout={0}
+                      transitionAppearTimeout={5000}>
+                      <p className="item">
+                        <Icon type='story' folder='activities' size='large' area='splash'/>
+                        <br />
+                        <Button bsSize="large" className="showStories" onClick={this.onClickStories}>
+                          <FormattedMessage id='takemetostories'/>
+                        </Button>
+                      </p>
+                    </ReactCSSTransitionGroup>
+                </Col>
+              </Row>
+            </div>
+          );
+          break;
+
+
+    }
 
   }
 
