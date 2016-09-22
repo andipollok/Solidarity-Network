ccced72869f2a81c466bba16b32f7cddf080d699
David Roulin <david.roulin@epfl.ch>
2016-09-22 17:34:16 +0200

    fixing rendering details in Main menu

--- src/components/Nav/MainMenu.js
+++ src/components/Nav/MainMenu.js
@@ -50,27 +50,27 @@ export default React.createClass({
         <div id="menu" onClick={this.onClickMenuFrame}>
 
             <Button className="UpcomingButton" size="bsLarge" onClickCapture={this.onClickUpcoming}>
-              <Icon type='upcoming' folder='service' size='large' isNav={true} isActive={true}/>
+              <Icon type='upcoming' folder='service' size='large' isActive={true} area="start"/>
               <FormattedMessage id='upcoming' />
             </Button>
 
             <Button className="CalendarButton" size="bsLarge" onClickCapture={this.onClickCalendar}>
-              <Icon type='calendar' folder='service' size='large' isNav={true} isActive={true}/>
+              <Icon type='calendar' folder='service' size='large' isActive={true} area="start"/>
               <FormattedMessage id='calendar' />
             </Button>
 
             <Button className="JournalButton" size="bsLarge" onClickCapture={this.onClickJournal}>
-              <Icon type='journal' folder='service' size='large' isNav={true} isActive={true}/>
+              <Icon type='journal' folder='service' size='large' isActive={true} area="start"/>
               <FormattedMessage id='journal' />
             </Button>
 
             <Button className="PhotoButton" size="bsLarge" onClickCapture={this.onClickPhoto}>
-              <Icon type='photo' folder='service' size='large' isNav={true} isActive={true}/>
+              <Icon type='photo' folder='service' size='large' isActive={true} area="start"/>
               <FormattedMessage id='photo' />
             </Button>
 
             <Button className="SettingsButton" size="bsLarge" onClickCapture={this.onClickSettings}>
-              <Icon type='settings' folder='service' size='large' isNav={true} isActive={true}/>
+              <Icon type='settings' folder='service' size='large' isActive={true} area="start"/>
               <FormattedMessage id='settings' />
             </Button>
 
