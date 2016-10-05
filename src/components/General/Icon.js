bd20975dfbf69f0dad94b9f8e3b0bd98c64f9ae1
Andreas Koller <mail@andreaskoller.com>
2016-10-05 16:09:02 +0200

    Added background to icon

--- src/components/General/Icon.js
+++ src/components/General/Icon.js
@@ -3,58 +3,35 @@ import SvgIcon from 'react-svg-icons';
 import classNames from 'classnames';
 
 var colors = {
-  // splash: {
-  //   backgroundColor: '#F6F6F6',
-  //   iconColor: '#FFF',
-  //   solidIconColor: '#FFF'
-  // },
+
   start: {
-    backgroundColor: 'transparent',
+    hasBackground: false,
     iconColor: '#FFF',
     solidIconColor: '#FFF'
   },
-  // news: {
-  //   backgroundColor: '#F6F6F6',
-  //   iconColor: '#e62719',
-  //   solidIconColor: '#823FC2'
-  // },
-  // activities: {
-  //   backgroundColor: '#F6F6F6',
-  //   iconColor: '#5CDAC3',
-  //   solidIconColor: '#823FC2'
-  // },
-  // stories: {
-  //   backgroundColor: '#F6F6F6',
-  //   iconColor: '#40bf4d',
-  //   solidIconColor: '#823FC2'
-  // },
+
   default: {
-    backgroundColor: 'transparent',
+    hasBackground: true,
     iconColor: '#823FC2',
     solidIconColor: '#823FC2'
-  },
-  // secondaryinfo: {
-  //   iconColor: '#b3b3b3'
-  // }
+  }
+
 }
 var inactiveColor = '#b3b3b3';
 
 export default React.createClass({
 
-  // shouldComponentUpdate: function(nextProps, nextState) {
-  //   return false;
-  // },
-
   render() {
     
     var data = this.props.data;
 
-    var backgroundColor = colors['default'].backgroundColor;
+    var hasBackground = colors['default'].hasBackground;
     var iconColor = colors['default'].iconColor;
-
+    
     // select background color and iconColor based on area (whatsnew, agenda or photos)
     if (data && data.status && data.status.page && colors[data.status.page]) {
-      backgroundColor = colors[data.status.page].backgroundColor;
+
+      hasBackground = colors[data.status.page].hasBackground;
       iconColor = colors[data.status.page].iconColor;
       // if fill parameter is solid (has solid background), then choose different icon color
       if (this.props.isOnSolid === true) {
@@ -68,13 +45,12 @@ export default React.createClass({
       iconColor = inactiveColor;
     }
 
-    // iconColor = "#823FC2";
-
     var divClass = classNames('icon', {
       tiny: this.props.size === 'tiny',
       small: this.props.size === 'small',
       medium: this.props.size === 'medium',
       large: this.props.size === 'large',
+      filled: hasBackground
     });
 
     var folder = this.props.folder || 'activities';
