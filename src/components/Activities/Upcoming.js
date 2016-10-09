b57e7971afd01e59c553e2e4b298653f35db60eb
Andreas Koller <mail@andreaskoller.com>
2016-10-09 18:43:55 +0200

    Show "Upcoming" as title

--- src/components/Activities/Upcoming.js
+++ src/components/Activities/Upcoming.js
@@ -29,7 +29,7 @@ export default React.createClass({
   componentWillMount() {
     StatusActions.setPage('activities');
     StatusActions.showBackButton(false);
-    StatusActions.setTitle(<FormattedMessage id='nav_activities' />);
+    StatusActions.setTitle(<FormattedMessage id='nav_upcoming' />);
     //StatusActions.setSecondaryNav(<ViewSelectorButtons data={this.props.data} view='upcoming'/>);
     StatusActions.forceTrigger();
   },
